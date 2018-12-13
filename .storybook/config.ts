307c9c20d99829a0bf297b062ab6f5a2251ca096
Devin Fee <devin@devinfee.com>
2018-12-13 12:05:01 -0800

    updated storybook config

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,25 +1,98 @@
-import { checkA11y } from "@storybook/addon-a11y";
-import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
 import { setOptions } from "@storybook/addon-options";
+import {
+  configureViewport,
+  INITIAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
 import { addDecorator, configure } from "@storybook/react";
 
 import "../src/index.module.sass";
 
+// addon-info is currently broken with prop types
+// import { withInfo } from "@storybook/addon-info";
+// addDecorator(
+//   withInfo({
+//     // propTables: false,
+//     source: false,
+//   }),
+// );
+
 setOptions({
-  name: "RBX",
+  name: "👟 rbx - Bulma UI",
   showAddonPanel: true,
   url: "https://github.com/dfee/rbx",
 });
 
-addDecorator(
-  withInfo({
-    // propTables: false,
-    source: false,
-  }),
-);
 addDecorator(withKnobs);
-addDecorator(checkA11y);
+
+configureViewport({
+  defaultViewport: "responsive",
+  // tslint:disable:object-literal-sort-keys
+  viewports: {
+    mobileSmall: {
+      name: "Mobile @ 480px",
+      styles: {
+        width: "480px",
+      },
+    },
+    mobileMax: {
+      name: "Mobile @ 768px (max)",
+      styles: {
+        width: "768px",
+      },
+    },
+    tabletMin: {
+      name: "Tablet @ 769px (min)",
+      styles: {
+        width: "769px",
+      },
+    },
+    tabletMax: {
+      name: "Tablet @ 1023px (max)",
+      styles: {
+        width: "1023px",
+      },
+    },
+    desktopMin: {
+      name: "Desktop @ 1024px (min)",
+      styles: {
+        width: "1024px",
+      },
+    },
+    desktopMax: {
+      name: "Desktop @ 1215px (max)",
+      styles: {
+        width: "1215px",
+      },
+    },
+    widescreenMin: {
+      name: "Widescreen @ 1216px (min)",
+      styles: {
+        width: "1216px",
+      },
+    },
+    widescreenMax: {
+      name: "Widescreen @ 1407px (max)",
+      styles: {
+        width: "1407px",
+      },
+    },
+    fullhdMin: {
+      name: "FullHD @ 1408px (min)",
+      styles: {
+        width: "1408px",
+      },
+    },
+    fullhdMax: {
+      name: "FullHD @ 2880px",
+      styles: {
+        width: "2880px",
+      },
+    },
+    ...INITIAL_VIEWPORTS,
+  },
+  // tslint:enable:object-literal-sort-keys
+});
 
 function loadStories() {
   const req = require.context("../stories", true, /\.story\.tsx$/);
--- src/base/generic.tsx
+++ src/base/generic.tsx
@@ -5,7 +5,10 @@ import { ModifierProps, transformModifiers } from "@/modifiers";
 
 export type GenericProps = ModifierProps;
 
-export const Generic = forwardRefAs<GenericProps, "div">((props, ref) => {
-  const { as, ...rest } = transformModifiers(props);
-  return React.createElement(as!, { ref, ...rest });
-}, { as: "div" });
+export const Generic = forwardRefAs<GenericProps, "div">(
+  (props, ref) => {
+    const { as, ...rest } = transformModifiers(props);
+    return React.createElement(as!, { ref, ...rest });
+  },
+  { as: "div" },
+);
